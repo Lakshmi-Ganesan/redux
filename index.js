a233a758afa038d1c418382692e01656c9fe293c
lakshmipriyal <93819132+lakshmipriyal@users.noreply.github.com>
2021-12-13 09:38:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import {createStore} from 'redux';
-import {combineReducers} from 'redux';
-import {reducer as formReducer} from 'redux-form'
-const store = createStore(combineReducers({
-    form: formReducer
-}));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+//import { Provider } from 'react-redux';
 
-export default store;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
